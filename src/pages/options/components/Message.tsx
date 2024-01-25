c063b1307e4567d0de5d46921709dfdd8b851616
LemonkaPix <maciejwrobel029@gmail.com>
2024-01-25 11:04:48 +0100

    Adding stylized code snippets and breaklines to messages

--- src/pages/options/components/Message.tsx
+++ src/pages/options/components/Message.tsx
@@ -1,10 +1,57 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import ReactHtmlParser from 'react-html-parser';
 
 export default function ChatMessage(props) {
   const { text, isUser } = props.message;
 
   const messageClass = isUser ? 'sent' : 'received';
+
+  function replaceNewLines(input: string): string {
+    return input.replace(/\n/g, '<br>');
+  }
+
+  function FormatText(_text) {
+    let messageText = replaceNewLines(_text);
+    return messageText;
+  }
+  const transformString = (str) => {
+    const codeSnippets = [];
+    let index = 0;
+
+    while (index !== -1) {
+      index = str.indexOf("```", index);
+      if (index === -1) break;
+
+      const openingIndex = index;
+      index = str.indexOf("```", index + 3);
+      const closingIndex = index !== -1 ? index : str.length;
+      const codeSnippet = str.slice(openingIndex + 3, closingIndex);
+      index += 3;
+      codeSnippets.push(codeSnippet);
+    }
+
+    const content = str.split("```");
+
+    return (
+      <>
+        {content.map((content, index) => (
+          (index % 2 != 0) ?
+            <CodeMirror
+              value={content}
+              // height="200px"
+              theme="dark"
+              extensions={[javascript({ jsx: true })]}
+              readOnly={true}
+
+            /> : <div dangerouslySetInnerHTML={{ __html: FormatText(content) }} />
+
+        ))}
+      </>
+    );
+  };
 
   return (
     <>
@@ -18,7 +65,7 @@ export default function ChatMessage(props) {
           sx={{
             wordWrap: 'break-word',
           }}>
-          {text}
+          {transformString(text)}
         </Typography>
       </Box>
     </>
